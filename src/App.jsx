4829e94c21747f90c39e18dad05ca90994c6f56c
nattapong_2005 <azzzx30089@gmail.com>
2024-11-08 12:29:15 +0700

    'fixed'

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ const App = () => {
   };
 
   const button = (amount) => {
-    setWithdrawAmount(amount + withdrawAmount);
+    setWithdrawAmount((prevAmount) => (prevAmount ? (parseInt(prevAmount) + amount).toString() : amount.toString()));
   };
 
 
